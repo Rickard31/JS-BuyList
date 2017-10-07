b1f389a1d6c66b828623bed373728f0ee5250d18
Rickard31 <vitalka.marchenko@gmail.com>
2017-10-07 12:41:12 +0300

    HomeWork3

--- js/main.js
+++ js/main.js
@@ -1,120 +1,7 @@
 $(function () {
 
     $("#add-item").click(function () {
-        var rowTemplate = $($("#row-template").html());
-        rowTemplate.find(".itemName").text($("#new-item-name").val());
-        console.log(rowTemplate);
-        $(".leftColumn").append(rowTemplate);
-
-        rowTemplate.find(".itemName").click(function () {
-            var v = $(this).text();
-            $(this).css("display", "none");
-            $(this).parent().find('.hidden-input').attr("value", v);
-            $(this).parent().find('.hidden-input').css("display", 'inline-block');
-            $(this).parent().find('.hidden-input').focus();
-        });
-        rowTemplate.find(".hidden-input").focusout(function () {
-            var text = $(this).val();
-            $(this).parent().find(".itemName").text(text);
-            $(this).parent().find(".itemName").css("display", "inline-block");
-            $(this).css('display', 'none');
-
-            var t = $(this).parent().index();
-            var counter = 1, res = 0;
-            $('.leftColumn').children(".otherLeftLines").each(function () {
-                if (counter == t) return false;
-                if (!$(this).hasClass(".is-bought")) res++;
-                counter++;
-            });
-            var el = $(this).parent().parent().parent().parent().find("#to-buy-elements-line").children().get(res);
-            $(el).find(".rightColumnElementName").text(text);
-        });
-        rowTemplate.find(".increaseItemAmountButton").click(function () {
-            var i = $(this).parent().find(".itemAmountCounter").text();
-            i++;
-            $(this).parent().find(".itemAmountCounter").fadeOut(250, function () {
-                $(this).text(i);
-                $(this).fadeIn(250);
-            });
-            if (i == 2) {
-                $(this).parent().find(".reduceItemAmountButton").removeAttr('disabled');
-                $(this).parent().find(".reduceItemAmountButton").addClass("tooltip");
-                $(this).parent().find(".reduceItemAmountButton").css('opacity', 1);
-            }
-            var t = $(this).parent().parent().index();
-            var counter = 1, res = 0;
-            $('.leftColumn').children(".otherLeftLines").each(function () {
-                if (counter == t) return false;
-                if (!$(this).hasClass(".is-bought")) res++;
-                counter++;
-            });
-            var el = $(this).parent().parent().parent().parent().find("#to-buy-elements-line").children().get(res);
-            $(el).find(".rightColumnElementAmount").text(i);
-        });
-        rowTemplate.find(".reduceItemAmountButton").click(function () {
-            var i = $(this).parent().find(".itemAmountCounter").text();
-            i--;
-            $(this).parent().find(".itemAmountCounter").fadeOut(250, function () {
-                $(this).text(i);
-                $(this).fadeIn(250);
-            });
-            if (i == 1) {
-                $(this).prop('disabled', true);
-                $(this).removeClass("tooltip");
-                $(this).css('opacity', 0.5);
-            }
-
-            var t = $(this).parent().parent().index();
-            var counter = 1, res = 0;
-            $('.leftColumn').children(".otherLeftLines").each(function () {
-                if (counter == t) return false;
-                if (!($(this).hasClass(".is-bought"))) res++;
-                counter++;
-            });
-            var el = $(this).parent().parent().parent().parent().find("#to-buy-elements-line").children().get(res);
-            $(el).find(".rightColumnElementAmount").text(i);
-        });
-        rowTemplate.find(".boughtButton").click(function () {
-            var row = $(this).parent().parent();
-            row.addClass(".is-bought");
-            row.find(".itemName").css('text-decoration', "line-through");
-            row.find(".reduceItemAmountButton").css('display', 'none');
-            row.find(".increaseItemAmountButton").css('display', 'none');
-            row.find(".removeButton").css('display', 'none');
-            row.find(".boughtButton").css('display', 'none');
-            row.find(".unbuy-button").css('display', 'inline-block');
-            resetRightColumn();
-        });
-        rowTemplate.find(".unbuy-button").click(function () {
-            var row = $(this).parent().parent();
-            row.removeClass(".is-bought");
-            row.find(".itemName").css('text-decoration', "none");
-            row.find(".reduceItemAmountButton").css('display', 'inline-block');
-            row.find(".increaseItemAmountButton").css('display', 'inline-block');
-            row.find(".removeButton").css('display', 'inline-block');
-            row.find(".boughtButton").css('display', 'inline-block');
-            row.find(".unbuy-button").css('display', 'none');
-            resetRightColumn();
-        });
-        rowTemplate.find(".removeButton").click(function () {
-            var row = $(this).parent().parent();
-            var t = row.index();
-            var counter = 1, res = 0;
-            $('.leftColumn').children(".otherLeftLines").each(function () {
-                if (counter == t) return false;
-                if (!($(this).hasClass(".is-bought"))) res++;
-                counter++;
-            });
-            row.remove();
-
-            var el = $($("#to-buy-elements-line").children().get(res));
-            el.remove();
-        });
-
-        var elementTemplate = $($("#element-template").html());
-        elementTemplate.find(".rightColumnElementName").text($("#new-item-name").val());
-        $("#to-buy-elements-line").append(elementTemplate);
-        $("#new-item-name").prop('value', "");
+        addItem($('#new-item-name').val());
     });
 
     $(".itemName").click(function () {
@@ -250,6 +137,123 @@ $(function () {
                 $("#bought-elements-line").append(elementTemplate);
             }
         });
-    }
+    };
+
+    function addItem(name) {
+        var rowTemplate = $($("#row-template").html());
+        rowTemplate.find(".itemName").text(name);
+        console.log(rowTemplate);
+        $(".leftColumn").append(rowTemplate);
+
+        rowTemplate.find(".itemName").click(function () {
+            var v = $(this).text();
+            $(this).css("display", "none");
+            $(this).parent().find('.hidden-input').attr("value", v);
+            $(this).parent().find('.hidden-input').css("display", 'inline-block');
+            $(this).parent().find('.hidden-input').focus();
+        });
+        rowTemplate.find(".hidden-input").focusout(function () {
+            var text = $(this).val();
+            $(this).parent().find(".itemName").text(text);
+            $(this).parent().find(".itemName").css("display", "inline-block");
+            $(this).css('display', 'none');
+
+            var t = $(this).parent().index();
+            var counter = 1, res = 0;
+            $('.leftColumn').children(".otherLeftLines").each(function () {
+                if (counter == t) return false;
+                if (!$(this).hasClass(".is-bought")) res++;
+                counter++;
+            });
+            var el = $(this).parent().parent().parent().parent().find("#to-buy-elements-line").children().get(res);
+            $(el).find(".rightColumnElementName").text(text);
+        });
+        rowTemplate.find(".increaseItemAmountButton").click(function () {
+            var i = $(this).parent().find(".itemAmountCounter").text();
+            i++;
+            $(this).parent().find(".itemAmountCounter").fadeOut(250, function () {
+                $(this).text(i);
+                $(this).fadeIn(250);
+            });
+            if (i == 2) {
+                $(this).parent().find(".reduceItemAmountButton").removeAttr('disabled');
+                $(this).parent().find(".reduceItemAmountButton").addClass("tooltip");
+                $(this).parent().find(".reduceItemAmountButton").css('opacity', 1);
+            }
+            var t = $(this).parent().parent().index();
+            var counter = 1, res = 0;
+            $('.leftColumn').children(".otherLeftLines").each(function () {
+                if (counter == t) return false;
+                if (!$(this).hasClass(".is-bought")) res++;
+                counter++;
+            });
+            var el = $(this).parent().parent().parent().parent().find("#to-buy-elements-line").children().get(res);
+            $(el).find(".rightColumnElementAmount").text(i);
+        });
+        rowTemplate.find(".reduceItemAmountButton").click(function () {
+            var i = $(this).parent().find(".itemAmountCounter").text();
+            i--;
+            $(this).parent().find(".itemAmountCounter").fadeOut(250, function () {
+                $(this).text(i);
+                $(this).fadeIn(250);
+            });
+            if (i == 1) {
+                $(this).prop('disabled', true);
+                $(this).removeClass("tooltip");
+                $(this).css('opacity', 0.5);
+            }
+
+            var t = $(this).parent().parent().index();
+            var counter = 1, res = 0;
+            $('.leftColumn').children(".otherLeftLines").each(function () {
+                if (counter == t) return false;
+                if (!($(this).hasClass(".is-bought"))) res++;
+                counter++;
+            });
+            var el = $(this).parent().parent().parent().parent().find("#to-buy-elements-line").children().get(res);
+            $(el).find(".rightColumnElementAmount").text(i);
+        });
+        rowTemplate.find(".boughtButton").click(function () {
+            var row = $(this).parent().parent();
+            row.addClass(".is-bought");
+            row.find(".itemName").css('text-decoration', "line-through");
+            row.find(".reduceItemAmountButton").css('display', 'none');
+            row.find(".increaseItemAmountButton").css('display', 'none');
+            row.find(".removeButton").css('display', 'none');
+            row.find(".boughtButton").css('display', 'none');
+            row.find(".unbuy-button").css('display', 'inline-block');
+            resetRightColumn();
+        });
+        rowTemplate.find(".unbuy-button").click(function () {
+            var row = $(this).parent().parent();
+            row.removeClass(".is-bought");
+            row.find(".itemName").css('text-decoration', "none");
+            row.find(".reduceItemAmountButton").css('display', 'inline-block');
+            row.find(".increaseItemAmountButton").css('display', 'inline-block');
+            row.find(".removeButton").css('display', 'inline-block');
+            row.find(".boughtButton").css('display', 'inline-block');
+            row.find(".unbuy-button").css('display', 'none');
+            resetRightColumn();
+        });
+        rowTemplate.find(".removeButton").click(function () {
+            var row = $(this).parent().parent();
+            var t = row.index();
+            var counter = 1, res = 0;
+            $('.leftColumn').children(".otherLeftLines").each(function () {
+                if (counter == t) return false;
+                if (!($(this).hasClass(".is-bought"))) res++;
+                counter++;
+            });
+            row.remove();
+
+            var el = $($("#to-buy-elements-line").children().get(res));
+            el.remove();
+        });
+
+        var elementTemplate = $($("#element-template").html());
+        elementTemplate.find(".rightColumnElementName").text(name);
+        $("#to-buy-elements-line").append(elementTemplate);
+        $("#new-item-name").prop('value', "");
+    };
 });
 
